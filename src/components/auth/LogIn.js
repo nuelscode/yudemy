46cf4fdf1ce1467226268c9339e04fa043f314db
engreric <okemmadueric@gmail.com>
2020-04-17 23:19:51 +0100

    fix signup and login

--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -1,91 +1,82 @@
-import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import styled from 'styled-components';
-
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import styled from "styled-components";
 
 export class LogIn extends Component {
-        userData;
-    
-        constructor(props) {
-            super(props);
-            this.onChangeEmail = this.onChangeEmail.bind(this);
-            this.onChangePhone = this.onChangePassword.bind(this);
-            this.onSubmit = this.onSubmit.bind(this);
-    
-            this.state = {
-                email: '',
-                password: ''
-            }
-        }
-    
-        // Form Values
+  constructor(props) {
+    super(props);
+    this.onValueChange = this.onValueChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-        onChangeEmail(e) {
-            this.setState({ email: e.target.value })
-        }
-    
-        onChangePassword(e) {
-            this.setState({ password: e.target.value })
-        }
-    
-    
-        // React Life Cycle
-        componentDidMount() {
-            this.userData = JSON.parse(localStorage.getItem('user'));
-    
-            if (localStorage.getItem('user')) {
-                this.setState({
-                    email: this.userData.email,
-                    password: this.userData.password
-                })
-            } else {
-                this.setState({
-                    email: '',
-                    password: ''
-                })
-            }
-        }
-    
-        componentWillUpdate(nextProps, nextState) {
-            localStorage.setItem('user', JSON.stringify(nextState));
-        }
-    
-        onSubmit(e) {
-            e.preventDefault()
+    this.state = {};
+  }
 
-            this.setState({
-                email: '',
-                password: ''
-            })
-            
-            console.log(this.state.props)
-        }
+  // Form Values
+  onValueChange(e) {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    render () {
+  handleSubmit(e) {
+    e.preventDefault();
 
-        return (
+    localStorage.setItem("user", JSON.stringify({ ...this.state })) &&
+      this.setState({});
+  }
 
-        <div className="container">
-           <Form onSubmit={this.onSubmit}>
-             <Form.Group controlId="formBasicFirstName">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={this.onChangeEmail} />
-              <Form.Text className="text-muted">
-                 We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+  // React Life Cycle
+  componentDidMount() {
+    this.userData = JSON.parse(localStorage.getItem("user"));
 
-            <Form.Group controlId="formBasicPassword">
-             <Form.Label>Password</Form.Label>
-             <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.onChangePassword} />
-            </Form.Group>
-            <Button type="submit" className="btn btn-primary btn-block">
-             Log in
-            </Button>
-           </Form>
-        </div>
+    if (localStorage.getItem("user")) {
+      //IF USER IS FOUND, ROUTE USER TO HOME PAGE
+    } else {
+      // STAY IN THE LOGIN PAGE SO USER CAN LOGIN
+      this.setState({
+        email: "",
+        password: "",
+      });
+    }
+  }
 
-       )
-   }
+  render() {
+    return (
+      <div className="container">
+        <Form>
+          <Form.Group controlId="formBasicFirstName">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={this.onValueChange}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.onValueChange}
+            />
+          </Form.Group>
+          <Button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={this.handleSubmit}
+          >
+            Log in
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,122 +1,105 @@
-import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import styled from 'styled-components';
-
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import styled from "styled-components";
 
 export class SignUp extends Component {
-        userData;
-    
-        constructor(props) {
-            super(props);
-    
-            this.onChangeName = this.onChangeFirstName.bind(this);
-            this.onChangeName = this.onChangeLastName.bind(this);
-            this.onChangeEmail = this.onChangeEmail.bind(this);
-            this.onChangePhone = this.onChangePassword.bind(this);
-            this.onSubmit = this.onSubmit.bind(this);
-    
-            this.state = {
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: ''
-            }
-        }
-    
-        // Form Values
-        onChangeFirstName(e) {
-            this.setState({ firstName: e.target.value })
-        }
+  userData;
 
-        onChangeLastName(e) {
-            this.setState({ lastName: e.target.value })
-        }
-    
-        onChangeEmail(e) {
-            this.setState({ email: e.target.value })
-        }
-    
-        onChangePassword(e) {
-            this.setState({ password: e.target.value })
-        }
-    
-    
-        // React Life Cycle
-        componentDidMount() {
-            this.userData = JSON.parse(localStorage.getItem('user'));
-    
-            if (localStorage.getItem('user')) {
-                this.setState({
-                    firstName: this.userData.firstName,
-                    lastnName: this.userData.lastName,
-                    email: this.userData.email,
-                    password: this.userData.password
-                })
-            } else {
-                this.setState({
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    password: ''
-                })
-            }
-        }
-    
-        componentWillUpdate(nextProps, nextState) {
-            localStorage.setItem('user', JSON.stringify(nextState));
-        }
-    
-        onSubmit(e) {
-            e.preventDefault()
+  constructor(props) {
+    super(props);
 
-            this.setState({
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: ''
-            })
+    this.onValueChange = this.onValueChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-            console.log(this.state.props)
-        }
+    this.state = {};
+  }
 
-    render () {
+  // Form Values
+  onValueChange(e) {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-        return (
+  // React Life Cycle
+  componentDidMount() {
+    this.userData = JSON.parse(localStorage.getItem("user"));
 
-            <div className="container">
-            <Form onSubmit={this.onSubmit}>
-             <Form.Group controlId="formBasicFirstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="firstName" placeholder="First Name" value={this.state.firstname} onChange={this.onChangeFirstName} />
+    if (localStorage.getItem("user")) {
+      //IF USER IS FOUND, ROUTE USER TO HOME PAGE
+    } else {
+      // STAY IN THE LOGIN PAGE SO USER CAN LOGIN
+      this.setState({});
+    }
+  }
 
-            </Form.Group>
-             <Form.Group controlId="formBasicLastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="lastName" placeholder="Last Name" value={this.state.lastname} onChange={this.onChangeLastName} />
+  onSubmit(e) {
+    e.preventDefault();
+    // On submit, i am storing the user details in the local storage
+    // And this user details is found in the local state
+    // The Proper thing to do is to send the data to the signup API
+    localStorage.setItem("user", JSON.stringify({ ...this.state }));
+  }
 
-            </Form.Group>
-             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={this.onChangeEmail} />
-              <Form.Text className="text-muted">
-                 We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+  render() {
+    return (
+      <div className="container">
+        <Form>
+          <Form.Group controlId="formBasicFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={this.state.firstname}
+              onChange={this.onValueChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              value={this.state.lastname}
+              onChange={this.onValueChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={this.onValueChange}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-             <Form.Label>Password</Form.Label>
-             <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.onChangePassword} />
-            </Form.Group>
-            <Button type="submit" className="btn btn-primary btn-block">
-             Sign up
-            </Button>
-           </Form>
-            </div>
-
-       )
-   }
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.onValueChange}
+            />
+          </Form.Group>
+          <Button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={this.onSubmit}
+          >
+            Sign up
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
-
-
-
