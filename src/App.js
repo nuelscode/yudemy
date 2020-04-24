5f565edcdbeee5684ad17f9e3ed2911b9398909f
nuelscode <chinagorom.onwubiko@gmail.com>
2020-04-23 22:50:48 -0700

    Search bar not retrieving data

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./Home";
-import { Categories } from "./Categories";
+import { Home } from "./components/Home";
+import { Categories } from "./components/Categories";
 import { SignUp } from './components/auth/SignUp';
-import { TeachOnYudemy } from "./TeachOnYudemy";
-import { YudemyForBusiness } from "./YudemyForBusiness";
-import { NoMatch } from "./NoMatch";
+import { TeachOnYudemy } from "./components/TeachOnYudemy";
+import { YudemyForBusiness } from "./components/YudemyForBusiness";
+import { NoMatch } from "./components/NoMatch";
 import { LogIn } from './components/auth/LogIn';
 import { Layout } from "./components/Layout";
 import NavigationBar from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
-import { SearchBar } from './SearchBar';
-import { VideoDetail } from '../components/VideoDetail';
-import { VideoList } from '../components/VideoList';
-import { Grid } from '@material-ui/core';
-import youtube from './api/youtube';
+import  SearchBar  from './components/SearchBar';
 
-
-const App = () => {
-
-  const [ videos, setVideos ] = useState([]);
-  const  [ selectedVideo, setSelectedVideo ] = useState(null);
-
-//  const handleSubmit = async (searchTerm) => {
-  //  const response = await youtube.get('search', { params: { q: searchTerm } });
-
-  //  setVideos(response.data.items)
-   // setSelectedVideo(response.data.items[0])
-
+function App() {
   return (
-    <Grid style={{ justifyContent: "center" }} container spacing={10}>
-      <Grid item xs={11}>
-        <Grid container spacing={10}>
-          <Grid item xs={12}>
-            <SearchBar onSubmit={handleSubmit} />
-          </Grid>
-          <Grid item xs={8}>
-            <VideoDetail video={selectedVideo} />
-          </Grid>
-          <Grid item xs={4}>
-            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
-          </Grid>
-
     <React.Fragment>
     <NavigationBar/>
     <Jumbotron/>
@@ -51,31 +23,16 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route path="/business" component={YudemyForBusiness} />
           <Route path="/teach" component={TeachOnYudemy} />
+          <Route path="/Categories" component={Categories} />
+          <Route path="/SearchBar" component={SearchBar} />
           <Route path="/login" component={LogIn} />
           <Route path="/signup" component={SignUp} />
           <Route component={NoMatch} />
         </Switch>
       </Router>
     </Layout>
-    </React.Fragment>
-        </Grid>
-      </Grid>
-    </Grid>  
+    </React.Fragment> 
   );
-
-  async function handleSubmit(searchTerm) {
-    const { data: { items: videos } } = await youtube.get("search", {
-      params: {
-        part: "snippet",
-        maxResults: 5,
-        key: 'AIzaSyBMvI_r-SvyENT5z7P8riBjcwlEe9Jote8',
-        q: searchTerm,
-      }
-    });
-
-    setVideos(videos);
-    setSelectedVideo(videos[0]);
-  }
 }
 
 export default App;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import styled from 'styled-components';
-import { SearchBar } from './SearchBar';
+import SearchBar from './SearchBar';
 
 
 const Styles = styled.div `
 navbar {
     background-color: #000
 }
-
 .navbar-brand, .navbar-nav, .navbar-link {
     color: #666;
-
     &:hover {
         color: white
     }
 }
 `;
+
 
  const NavigationBar = () =>(
     <Styles>
@@ -28,7 +27,7 @@ navbar {
                 <Nav className="ml-auto"  variant="primary">
                     <Nav.Item><Nav.Link href="/categories">Categories</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/business">Yudemy for Business</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/teach"><SearchBar /></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/search"><SearchBar/></Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/teach">Teach on Yudemy</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/login"><Button variant="secondary">Log in</Button></Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/signup"><Button variant="primary">Sign Up</Button></Nav.Link></Nav.Item>
